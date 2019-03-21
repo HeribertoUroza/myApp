f2f8a47189c3ff8a9bfcbc5bc2a7c43ead56fe16
Heriberto Uroza <heribertouroza@pursuit.org>
2019-03-20 20:51:13 -0400

    using heroku port

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ app.get('/ping', (req, res) => {
     res.json({'pong': true})
 })
 
-const port = 3000;
+const port = process.env.PORT || 5001;
 app.listen(port, () => {
     console.log(`server is listening on port: ${port}`)
 })
 
-//node index.js needed to start the the server
+//node index.js needed to start the the server,
+// we updated the package.json with the 'start' : 'index.js' inside scripts
+//
 

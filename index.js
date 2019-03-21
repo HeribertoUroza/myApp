0f3e16843d72cb3c31547532276879370a1c5fc2
Heriberto Uroza <heribertouroza@pursuit.org>
2019-03-20 20:23:56 -0400

    added files

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.get('/ping', (req, res) => {
+    res.status(200)
+    res.json({'pong': true})
+})
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`server is listening on port: ${port}`)
+})
